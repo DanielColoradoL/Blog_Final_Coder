ab7382a5977a32c3de97aad93b2b7c3c37dfc8fa
Daniel ColoradoL <danielcoloradol@hotmail.com>
2022-12-18 18:53:39 +1100

    inicio funciona, lee la base de datos y muestra los ultimos blogs creados, comenzando por los mas nuevos, super user creado tambien

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
-# class Post(models.Model):
 
-#     title = models.CharField(max_length=255)
-#     #body = models.TextField() 
-#     body = RichTextUploadingField() # CKEditor Rich Text Field
+class Post(models.Model):
+    titulo = models.CharField(max_length=255)
+    subtitulo = models.CharField(max_length=255)
+    cuerpo = RichTextUploadingField() # CKEditor Rich Text Field
+    imagen = models.ImageField(upload_to='media')
+    autor = models.CharField(max_length=255)
+    fecha = models.DateField()
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.titulo
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from blog.models import *
 
 def inicio(request):
-    return HttpResponse("Este es la pagina de inicio")
+    blogs = Post.objects.all().order_by('-id')[:4] # Consulta por id, y limita a 4 de mayor a menor (ORDER BY id DESC en SQL) (LIMIT 4)
+    if len(blogs) == 0:
+        blog_1 = Post.objects.none()
+        blog_2 = Post.objects.none()
+        blog_3 = Post.objects.none()
+        blog_4 = Post.objects.none()
+    elif len(blogs) == 1:
+        blog_1 = blogs[0]
+        blog_2 = Post.objects.none()
+        blog_3 = Post.objects.none()
+        blog_4 = Post.objects.none()
+    elif len(blogs) == 2:
+        blog_1 = blogs[0]
+        blog_2 = blogs[1]
+        blog_3 = Post.objects.none()
+        blog_4 = Post.objects.none()
+    elif len(blogs) == 3:
+        blog_1 = blogs[0]
+        blog_2 = blogs[1]
+        blog_3 = blogs[2]
+        blog_4 = Post.objects.none()
+    else:     
+        blog_1 = blogs[0]
+        blog_2 = blogs[1]
+        blog_3 = blogs[2]
+        blog_4 = blogs[3]
+    return render(request, "index.html", {"blog_1": blog_1, "blog_2": blog_2, "blog_3": blog_3, "blog_4": blog_4})
+
+
+# Post.objects.none() envia un queryset vacio
