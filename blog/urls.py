f62b6064a3abb862e6567b2a02160f920cd6567c
Daniel ColoradoL <danielcoloradol@hotmail.com>
2022-12-19 21:40:48 +1100

    Segundo commit, lectura post con ID

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from blog.views import *
 
 urlpatterns = [
     path("", inicio, name= "inicio"), 
+    path("post/<id>", post, name = "post"),
+    path("about", about_me, name= "about_me"),
+    path("allposts", all_posts, name= "all_posts"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import *
 
+
 def inicio(request):
     blogs = Post.objects.all().order_by('-id')[:4] # Consulta por id, y limita a 4 de mayor a menor (ORDER BY id DESC en SQL) (LIMIT 4)
     if len(blogs) == 0:
@@ -28,7 +29,17 @@ def inicio(request):
         blog_2 = blogs[1]
         blog_3 = blogs[2]
         blog_4 = blogs[3]
-    return render(request, "index.html", {"blog_1": blog_1, "blog_2": blog_2, "blog_3": blog_3, "blog_4": blog_4})
-
-
+    return render(request, "inicio.html", {"blog_1": blog_1, "blog_2": blog_2, "blog_3": blog_3, "blog_4": blog_4}) 
 # Post.objects.none() envia un queryset vacio
+
+
+def post(request, id):
+    lectura_publicacion = Post.objects.get(id = id)
+    return render(request, "post.html", {"lectura_publicacion" : lectura_publicacion})
+
+def all_posts(request):
+    blogs = Post.objects.all().order_by('-id') # Consulta por id, de mayor a menor (ORDER BY id DESC en SQL)
+    return render(request, "publicaciones.html", {"blogs": blogs}) 
+
+def about_me(request):
+    return render(request, "about.html")
